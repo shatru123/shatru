d3c1bebfadcf2918c96f6e16f2447d476acc9601
Shatrusonu <ambhoreshatrughna@gmail.com>
2017-10-02 22:32:08 +0530

    Add files via upload

---
+++ ReverseNo.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class ReverseNo
+ { 
+    public static void main(String args[]) throws IOException 
+	{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter string: ");
+        String s = br.readLine();
+		String[] arr = s.split(" ");    
+       // String reverse = "";
+		for ( String ss : arr)
+			{
+				
+				int length = ss.length();
+				//int lastchar=length - 1;
+				String reverse = "";
+				for (int i = length - 1; i >= 0; i--)
+				{
+				reverse = reverse + ss.charAt(i);
+				//System.out.print(ss.charAt(0)+ reverse+ss.charAt(lastchar)+" ");
+				}
+				//System.out.print(ss.charAt(0)+ reverse+ss.charAt(lastchar)+" ");
+				System.out.print(reverse+" ");
+				/*length=0;
+				lastchar=0;*/
+			//System.out.println("Result2:" +s.charAt(0)+ reverse+s.charAt(s.length()));
+			}
+
+		
+    } 
+}
