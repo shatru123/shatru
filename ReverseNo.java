027dfbdff56344d3f894d837964d4df73e9074ef
Shatrusonu <ambhoreshatrughna@gmail.com>
2017-10-02 22:36:28 +0530

    Update ReverseNo.java

--- ReverseNo.java
+++ ReverseNo.java
@@ -1,3 +1,4 @@
+/*Progrm to Reverse String Word wise*/
 import java.io.*;
 class ReverseString
  { 
@@ -7,25 +8,18 @@ class ReverseString
         System.out.print("Enter string: ");
         String s = br.readLine();
 		String[] arr = s.split(" ");    
-       // String reverse = "";
+      
 		for ( String ss : arr)
 			{
 				
 				int length = ss.length();
-				//int lastchar=length - 1;
+				
 				String reverse = "";
 				for (int i = length - 1; i >= 0; i--)
 				{
 				reverse = reverse + ss.charAt(i);
-				//System.out.print(ss.charAt(0)+ reverse+ss.charAt(lastchar)+" ");
 				}
-				//System.out.print(ss.charAt(0)+ reverse+ss.charAt(lastchar)+" ");
 				System.out.print(reverse+" ");
-				/*length=0;
-				lastchar=0;*/
-			//System.out.println("Result2:" +s.charAt(0)+ reverse+s.charAt(s.length()));
 			}
-
-		
-    } 
+      } 
 }
